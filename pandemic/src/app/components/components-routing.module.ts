5be81e9ad74153be9b206c9bcc98f589f32f8caa
Jajovise-11 <148467900+Jajovise-11@users.noreply.github.com>
2024-11-11 20:03:13 +0100

    commit2

--- pandemic/src/app/components/components-routing.module.ts
+++ pandemic/src/app/components/components-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 
 
@@ -10,7 +10,8 @@ const routes: Routes = [
 
 { path: '', component: LoginComponent },
 { path: 'login', component: LoginComponent },
-{ path: 'menu', component: MenuComponent }
+{ path: 'menu', component: MenuComponent },
+{ path: 'informacion', component: InformacionComponent}
 
 ];
 
---
+++ pandemic/src/app/components/informacion/informacion.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-informacion',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './informacion.component.html',
+  styleUrl: './informacion.component.css'
+})
+export class InformacionComponent {
+  informacion: string= "";
+
+  mostrarInformacion() {
+    this.informacion = '';
+  }
+}
