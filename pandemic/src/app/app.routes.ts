94433e54ab50be5b82f62c969ba018f491c5a1a7
Jajovise-11 <148467900+Jajovise-11@users.noreply.github.com>
2024-12-13 20:06:56 +0100

    si

--- pandemic/src/app/app.routes.ts
+++ pandemic/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
     { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule) },
-    { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
+    { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) }
 
     ];
